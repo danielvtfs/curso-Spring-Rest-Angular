081c102e0c448f3d8431aec07285db1de30afb50
danielvtfs <danielvtfs@gmail.com>
2018-10-26 11:10:34 -0300

    primeiro commit curso spring,rest,angular

---
+++ src/main/java/com/example/algamoney/api/event/listener/RecursoCriadoListener.java
@@ -0,0 +1,15 @@
+package com.example.algamoney.api.event.listener;
+
+import org.springframework.context.ApplicationListener;
+
+import com.example.algamoney.api.event.RecursoCriadoEvent;
+
+public class RecursoCriadoListener implements ApplicationListener<RecursoCriadoEvent>{
+
+	@Override
+	public void onApplicationEvent(RecursoCriadoEvent recursoCriadoEvent) {
+		
+		
+	}
+
+}
